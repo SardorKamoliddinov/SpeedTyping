d37c8dd6b5794fd64e46078338e671f0d4208caa
Kamoliddinov Sardor <111947544+KSardor@users.noreply.github.com>
2024-03-25 09:15:42 +0500

    added style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
-
 const App = () => {
   return (
-    <div>
-      <h1 className="text-center text-4xl">Speed Type</h1>
+    <div className="text-center bg-black text-white w-full h-screen">
+      <h1 className="text-2xl py-5">How many words can you type?</h1>
+      <textarea
+        cols="50"
+        rows="10"
+        className="text-black text-xl p-2"
+        disabled
+      ></textarea>
+      <h2 className="text-xl py-3">Time remaining: </h2>
+      <button className="m-5 px-7 py-2 bg-[#00b800] text-white">Start</button>
+      <h2 className="text-xl">Word Count: </h2>
     </div>
   );
 };

0e82087fcdc7baf69ad2a0be5457ffb4d7e4a8d8
Kamoliddinov Sardor <111947544+KSardor@users.noreply.github.com>
2024-03-25 15:26:14 +0500

    finished SpeedTyping game

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
+import React from "react";
+import SpeedTyping from "./component/SpeedTyping/SpeedTyping";
+
 const App = () => {
   return (
-    <div className="text-center bg-black text-white w-full h-screen">
-      <h1 className="text-2xl py-5">How many words can you type?</h1>
-      <textarea
-        cols="50"
-        rows="10"
-        className="text-black text-xl p-2"
-        disabled
-      ></textarea>
-      <h2 className="text-xl py-3">Time remaining: </h2>
-      <button className="m-5 px-7 py-2 bg-[#00b800] text-white">Start</button>
-      <h2 className="text-xl">Word Count: </h2>
+    <div>
+      <SpeedTyping />
     </div>
   );
 };
---
+++ src/component/SpeedTyping/SpeedTyping.js
@@ -0,0 +1,67 @@
+import { useState, useEffect, useRef } from "react";
+
+const SpeedTyping = () => {
+  const [typingAllowed, setTypingAllowed] = useState(false);
+  const [time, setTime] = useState(15);
+  const [wordCount, setWordCount] = useState(0);
+  const textareaRef = useRef(null);
+
+  const handleStartClick = () => {
+    setTypingAllowed(true);
+    setTime(15);
+    setWordCount(0);
+    textareaRef.current.focus();
+  };
+
+  const countWords = () => {
+    const text = textareaRef.current.value.trim();
+    if (text === "") {
+      setWordCount(0);
+    } else {
+      const words = text.split(/\s+/);
+      setWordCount(words.length);
+    }
+  };
+
+  useEffect(() => {
+    let interval;
+    if (typingAllowed) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+    }
+
+    if (time === 0) {
+      setTypingAllowed(false);
+      clearInterval(interval);
+      countWords();
+    }
+
+    return () => clearInterval(interval);
+  }, [typingAllowed, time]);
+
+  return (
+    <div className="text-center bg-black text-white w-full h-screen">
+      <h1 className="text-2xl py-5">How many words can you type?</h1>
+      <textarea
+        cols="50"
+        rows="10"
+        className="text-black text-xl p-2"
+        disabled={!typingAllowed}
+        ref={textareaRef}
+        onChange={countWords}
+      ></textarea>
+      <button
+        className="m-5 px-7 py-2 bg-[#00b800] text-white"
+        onClick={handleStartClick}
+        disabled={typingAllowed || time === 0}
+      >
+        Start
+      </button>
+      <h2 className="text-xl py-3">Time remaining: {time} seconds</h2>
+      {time === 0 && <h2 className="text-xl">Word Count: {wordCount}</h2>}
+    </div>
+  );
+};
+
+export default SpeedTyping;
